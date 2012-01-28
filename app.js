a31b9bbe42c158be05748983c75f38347ee5de91
tonakai <aykutsoysal@gmail.com>
2012-01-28 19:08:06 +0000

    setting up the modules

--- app.js
+++ app.js
@@ -59,3 +59,14 @@ io.sockets.on('connection', function(socket) {
         console.log(data);
     });
 });
+
+
+function captureThree() {
+    this.maxx = 6;
+    this.maxy = 6;
+    this.area = new Array(this.maxx);
+    for (var i=0;i<this.maxx;i++) {
+        this.area[i] = new Array(this.maxy);
+    }
+    
+}
