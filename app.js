585328fd0d18186595eb2539be04dbcae6200074
Aykut Soysal <aykutsoysal@gmail.com>
2012-08-22 23:31:49 +0200

    express 3 nowjs fixes are added

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 /**
  * Module dependencies.
  */
-
+var http = require('http');
 var express = require('express');
 var app = module.exports= express();
 var nowjs = require("now");
-var everyone;
 
 require('./public/core.js');
 
 // Configuration
 app.configure(function() {
+	app.set('port', process.env.PORT || 8080);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
     app.use(express.bodyParser());
@@ -48,9 +48,8 @@ app.get('/test', function(req, res) {
     });
 });
 
-// Only listen on $ node app.js
-if (!module.parent) {
-    app.listen(8080);
-    everyone = nowjs.initialize(app);
-    console.log("Express server listening on port %d", 8080);
-}
+var server = http.createServer(app).listen(app.get('port'), function() {
+console.log('Express server listening on port ' + app.get('port'));
+})
+
+var everyone = require('now').initialize(server);
